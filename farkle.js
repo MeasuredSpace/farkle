2c2670c9f4b6ae8fc3c4b7baab2b15449774726c
Brad Mead <measuredspace@gmail.com>
2024-02-27 15:48:39 -0700

    Added GameStartedEvent handler and related tests

--- farkle.js
+++ farkle.js
@@ -1,1 +1,14 @@
 
+function GameStartedEvent(playerCount) {
+  var _playerCount = playerCount < 0 ? 1 : playerCount;
+
+  this.getPlayerCount = function() {
+    return _playerCount;
+  };
+}
+module.exports = GameStartedEvent;
+
+
+
+
+
--- farkle.test.js
+++ farkle.test.js
@@ -1,3 +1,19 @@
+//Farkle tests
+
 test('adds 2 + 2 to equal 4', () => {
     expect(2 + 2).toBe(4);
+  });
+
+  const GameStartedEvent = require('./farkle.js');
+
+  test('GameStartedEvent should return correct player count', () => {
+    const playerCount = 4;
+    const gameStartedEvent = new GameStartedEvent(playerCount);
+    expect(gameStartedEvent.getPlayerCount()).toBe(playerCount);
+  });
+  
+  test('GameStartedEvent should not accept negative player counts', () => {
+    const playerCount = -1;
+    const gameStartedEvent = new GameStartedEvent(playerCount);
+    expect(gameStartedEvent.getPlayerCount()).not.toBe(playerCount);
   });
